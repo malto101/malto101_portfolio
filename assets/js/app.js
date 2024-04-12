2464dd3a9c95e4ff4e4e85c1f6feb81c31c2ef67
malto101 <dhruvmenon1104@gmail.com>
2024-04-12 15:35:15 +0530

    fix footer

--- assets/js/app.js
+++ assets/js/app.js
@@ -154,6 +154,11 @@ let footer = $(`
         </div>
     </div>
 
+
+    
+
+    <div class="rounded-social-buttons tag">
+
     <a class="social-button linkedin" href="" target="_blank" >
     <!-- svg code for linkedin icon -->
       <svg class="linkedin-icon-footer" xmlns="http://www.w3.org/2000/svg" width="35" viewBox="0 0 24 24" fill="#0e76a8"
